4e8d272dab228e6f2b03c548d17aa1fef3e6d84f
gowtham2803 <gowthamramasamy2804@gmail.com>
2026-02-10 23:10:00 +0530

    Practice Looping basics Problem Solving

---
+++ Java_Practices/Loop/EvenNumber.java
@@ -0,0 +1,11 @@
+// Print Even number 2 to 20
+
+public class EvenNumber{
+    public static void main(String [] args){
+
+        for(int i =2; i<=20; i=i+2){
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ Java_Practices/Loop/Table.java
@@ -0,0 +1,11 @@
+// Print 5th table upto 10
+
+public class Table{
+    public static void main(String [] args){
+
+        for(int i =1; i<=10; i++){
+            System.out.println(i+" "+"x5= "+i*5);
+        }
+
+    }
+}
